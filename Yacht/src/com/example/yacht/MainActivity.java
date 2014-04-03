24d1a80ae4156b4b8857397a95fd526deb2a0323
AndreeaMartinovici <andreea.martinovici@gmail.com>
2014-04-03 21:51:04 +0300

    Yacht - first steps
    Welcome activity
    Game activity - layout only

---
+++ Yacht/src/com/example/yacht/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.yacht;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	Button mBtnNewGame;
+	Button mBtnHowToPlay;
+	Context context;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mBtnNewGame = (Button) findViewById(R.id.btnNewGame);
+		mBtnHowToPlay = (Button) findViewById(R.id.btnHowPlay);
+		
+		context = this;
+		
+		mBtnNewGame.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(context, Game.class);
+				startActivity(i);
+			}
+		});
+		
+	}
+}
