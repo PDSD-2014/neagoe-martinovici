267aced49fc2290244d19bd07a6eee788d6a49b9
AndreeaMartinovici <andreea.martinovici@gmail.com>
2014-04-06 15:20:36 +0300

    Solving conflicts.

--- Yacht/src/com/example/yacht/Rules.java
+++ Yacht/src/com/example/yacht/Rules.java
@@ -19,15 +19,5 @@ public class Rules extends Activity {
 
 		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.rules, menu);
-		return true;
-	}
-
 	
-
 }
